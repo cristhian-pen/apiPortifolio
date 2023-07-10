f6736c5abf3d2fd331c2b68272b0a0dc461491c9
cristhian moura <cristhian_moura@live.com>
2023-07-10 13:23:54 -0300

    Versão 1.0
    .

--- App/Config/db.js
+++ App/Config/db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 //Conexão com o banco de dados
 const sequelize = new Sequelize('DB_NAME', 'DB_KEY', 'DB_AUTH', {
     host: 'DB_HOST',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: require('mysql2')
 });
 
 module.exports = sequelize;
