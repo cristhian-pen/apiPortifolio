f39f5e2e036171a9430086ace39d27f05b0ed508
cristhian moura <cristhian_moura@live.com>
2023-07-10 10:08:22 -0300

    Versão 1.0
    .

--- App/Config/db.js
+++ App/Config/db.js
@@ -2,14 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 //Conexão com o banco de dados
-const sequelize = new Sequelize(DB_NAME, DB_KEY, DB_AUTH, {
+const sequelize = new Sequelize('DB_NAME', 'DB_KEY', 'DB_AUTH', {
     host: DB_HOST,
     dialect: DB_DIALECT
 });
 
 module.exports = sequelize;
-/* 
-const sequelize = new Sequelize('portifolioDb', 'admins', 'Ironmharkv10#', {
-    host: 'srvportifoliodb.mysql.database.azure.com',
-    dialect: 'mysql'
-}); */
