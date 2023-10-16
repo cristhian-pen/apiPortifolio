3d9fd510f582bab21be4c1ef36708147acca5c00
cristhian-pen <cristhian_moura@live.com>
2023-10-15 23:46:43 -0300

    Versão 1.0

--- App/Config/db.js
+++ App/Config/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_KEY = process.env.DB_KEY;
+const DB_AUTH = process.env.DB_AUTH;
+const DB_HOST = process.env.DB_HOST;
+
+//require('dotenv').config();
 
 //Conexão com o banco de dados
-const sequelize = new Sequelize('DB_NAME', 'DB_KEY', 'DB_AUTH', {
-    host: 'srvportifoliodb.mysql.database.azure.com',
+const sequelize = new Sequelize(DB_NAME, DB_KEY, DB_AUTH, {
+    host: DB_HOST,
     dialect: 'mysql',
     dialectModule: require('mysql2')
 });
